fa4eed992a7807ba2b231d5868d2ebe7d1898be4
User <chalasaninagasaketh@gmail.com>
2020-02-26 20:33:42 +0530

    Interest_committed

---
+++ Saketh_Interest/src/main/java/Saketh_Interest/Saketh_Interest/App.java
@@ -0,0 +1,13 @@
+package Saketh_Interest.Saketh_Interest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
